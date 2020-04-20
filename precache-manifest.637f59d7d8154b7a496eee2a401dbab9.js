3a0bc94b78cbdf921e8d949aaed6d77d80bf37a7
Anthony Nguyen <anthonynguyen@anthonys-mbp.lan>
2020-04-19 20:59:08 -0700

    Updates

---
+++ precache-manifest.637f59d7d8154b7a496eee2a401dbab9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d78553d0dee2b1b19f01de88edd2ee0a",
+    "url": "/react-clicky-game/index.html"
+  },
+  {
+    "revision": "67c606bc9ea591b7c4a6",
+    "url": "/react-clicky-game/static/css/main.b840a2b6.chunk.css"
+  },
+  {
+    "revision": "96d9750153ca01801324",
+    "url": "/react-clicky-game/static/js/2.12430a71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-clicky-game/static/js/2.12430a71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "67c606bc9ea591b7c4a6",
+    "url": "/react-clicky-game/static/js/main.4e8ab864.chunk.js"
+  },
+  {
+    "revision": "f3cb2be7b7e1326c909a",
+    "url": "/react-clicky-game/static/js/runtime-main.fe3d3563.js"
+  }
+]);
